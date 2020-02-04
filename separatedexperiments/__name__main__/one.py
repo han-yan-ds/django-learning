d7ca2b6d8d9d6cc5abc580c6035585c4d70f3064
han-yan-ds <hanprogramming@gmail.com>
2020-02-04 12:36:18 -0700

    Added separatedexperiments folder, with __name__ == '__main__' example

---
+++ separatedexperiments/__name__main__/one.py
@@ -0,0 +1,8 @@
+"""
+Running one.py will reveal __name__ == "__main__"
+"""
+
+def printName():
+    print(__name__ + " is the function being run")
+
+printName()
---
+++ separatedexperiments/__name__main__/two.py
@@ -0,0 +1,7 @@
+"""
+Running two.py (and importing one.py) will reveal __name__ == "one"
+"""
+
+import one
+
+one.printName()
