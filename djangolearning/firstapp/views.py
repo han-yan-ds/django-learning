0e773ace8814e8c980212cd263bdbbec9b7e578a
han-yan-ds <hanprogramming@gmail.com>
2020-02-06 14:01:21 -0700

    small comment in view.py

--- djangolearning/firstapp/views.py
+++ djangolearning/firstapp/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(req):
+    # rendering from a template now
     templateDict = {'example_template': "Hello I am inserted from the HTML!"}
     return render(req, 'firstapp/index.html', context=templateDict)
 
