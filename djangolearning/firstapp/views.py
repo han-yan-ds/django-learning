de6c011f72751d7153cb2ff8c1485b9a27aca7d3
han-yan-ds <hanprogramming@gmail.com>
2020-02-06 13:59:33 -0700

    Added template rendering code in views.py

--- djangolearning/firstapp/views.py
+++ djangolearning/firstapp/views.py
@@ -3,7 +3,8 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(req):
-    return HttpResponse("BABY ONE MORE TIME")
+    templateDict = {'example_template': "Hello I am inserted from the HTML!"}
+    return render(req, 'firstapp/index.html', context=templateDict)
 
 def demoUrlMapping(req):
     return HttpResponse("Includes() works!!")
