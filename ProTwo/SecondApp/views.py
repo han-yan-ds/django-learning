71b92b642479e76b9b98b9cb0569b417108a4152
han-yan-ds <hanprogramming@gmail.com>
2020-02-06 17:20:20 -0700

    Put all of secondapp's templates under SecondApp folder

--- ProTwo/SecondApp/views.py
+++ ProTwo/SecondApp/views.py
@@ -7,17 +7,17 @@ def index(req):
         'help_link_text': 'Example of Rendering Text Template',
         'image_link_text': 'Example of Rendering Image Template',
     }
-    return render(req, 'index.html', context=templateDict)
+    return render(req, 'SecondApp/index.html', context=templateDict)
 
 
 def helpPage(req):
     templateDict = {
         'template_exercise':  "Help Page Text",
     }
-    return render(req, 'help.html', context=templateDict)
+    return render(req, 'SecondApp/help.html', context=templateDict)
 
 def imageDemo(req):
     templateDict = {
         'template_exercise':  "Demo-ing Image",
     }
-    return render(req, 'imageDemo.html', context=templateDict)
+    return render(req, 'SecondApp/imageDemo.html', context=templateDict)
