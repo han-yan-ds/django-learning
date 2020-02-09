89c69f3025391df44ec97f783aa7ad7a1fa7865d
han-yan-ds <hanprogramming@gmail.com>
2020-02-08 17:31:51 -0700

    Demo'd clean_fields and botcatcher

--- ProTwo/SecondApp/forms.py
+++ ProTwo/SecondApp/forms.py
@@ -4,3 +4,11 @@ class FormModel(forms.Form):
     name = forms.CharField()
     email = forms.EmailField()
     text = forms.CharField()
+    botCatcher = forms.CharField(widget=forms.HiddenInput, required=False) # this catches a bot because a bot will fill out this form, but a user won't (it's hidden)
+
+    def clean_botCatcher(self): # must be named clean_fieldName... look up "Django clean_fields"
+        botCatcherText = self.cleaned_data['botCatcher']
+        if len(botCatcherText) > 0:
+            raise forms.ValidationError("GOTCHA BOT!")
+        return botCatcherText
+        
