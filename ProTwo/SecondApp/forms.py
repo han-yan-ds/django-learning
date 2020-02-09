51ddfd8f094e49cb1d5ee961f8474af3fb3f4540
han-yan-ds <hanprogramming@gmail.com>
2020-02-08 17:40:43 -0700

    Demo'd using Django Validators on Forms.  THIS IS AWESOME

--- ProTwo/SecondApp/forms.py
+++ ProTwo/SecondApp/forms.py
@@ -1,14 +1,15 @@
 from django import forms
+from django.core import validators
 
 class FormModel(forms.Form):
     name = forms.CharField()
     email = forms.EmailField()
     text = forms.CharField()
-    botCatcher = forms.CharField(widget=forms.HiddenInput, required=False) # this catches a bot because a bot will fill out this form, but a user won't (it's hidden)
+    botCatcher = forms.CharField(widget=forms.HiddenInput, required=False, validators=[validators.MaxLengthValidator(0)]) # this catches a bot because a bot will fill out this form, but a user won't (it's hidden)
 
-    def clean_botCatcher(self): # must be named clean_fieldName... look up "Django clean_fields"
-        botCatcherText = self.cleaned_data['botCatcher']
-        if len(botCatcherText) > 0:
-            raise forms.ValidationError("GOTCHA BOT!")
-        return botCatcherText
+    # def clean_botCatcher(self): # must be named clean_fieldName... look up "Django clean_fields"
+    #     botCatcherText = self.cleaned_data['botCatcher']
+    #     if len(botCatcherText) > 0:
+    #         raise forms.ValidationError("GOTCHA BOT!")
+    #     return botCatcherText
         
