8355486cf90a7324ab03b2f02c132688e6dd1bfe
han-yan-ds <hanprogramming@gmail.com>
2020-02-11 08:11:36 -0700

    Added REST_FRAMEWORK to settings.py, defaulted pagination to 10 items

--- ProTwo/ProTwo/settings.py
+++ ProTwo/ProTwo/settings.py
@@ -125,3 +125,11 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     STATIC_DIR,
 ]
+
+
+# REST framework settings
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
+}
